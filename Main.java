d53c114ca38be0f7eb563de8f26ccb15ebe6e739
DenisBerezovski1 <119109483+DenisBerezovski1@users.noreply.github.com>
2023-04-21 22:45:47 +0300

    Add files via upload
    Урок 1. Принципы ООП: Инкапсуляция, наследование, полиморфизм

---
+++ Main.java
@@ -0,0 +1,25 @@
+package lesson1;
+import lesson1.TClasses.*;
+import lesson1.TClasses.GeneralClasses.Product;
+import lesson1.TClasses.RealClasses.*;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Product> myProduct = new ArrayList<>();
+        myProduct.add(new Bread("Батон", 1.02, 10,"шт", "30.10.2022", "Первый сорт"));
+        myProduct.add(new Diapers("Pampers", 250, 48, "шт", 2, "M", 10, 15, "M"));
+        myProduct.add(new Eggs("Золотые", 15, 10, "шт", "30.10.2022", 24));
+        myProduct.add(new Masks("Маски", 22, 100, "шт", 5));
+        myProduct.add(new Milk("ЭкоМилк", 10, 1, "литр", 1, 3.2, "30.10.2022"));
+        myProduct.add(new Pacifier("Соска", 25, 1, "шт"));
+        myProduct.add(new Soda("CocaCola", 10, 1, "литр", 1));
+        myProduct.add(new ToiletPapper("Zewa", 60, 8, "шт", 24, 3));
+
+        for(Product product: myProduct){
+            Program.getProductInfo(product);
+            System.out.println();
+        }
+    }
+}
---
+++ TClasses/Program.java
@@ -0,0 +1,9 @@
+package lesson1.TClasses;
+
+import lesson1.TClasses.GeneralClasses.Product;
+
+public class Program {
+   public static void getProductInfo(Product product){
+       System.out.println(product.toString());
+   }
+}
